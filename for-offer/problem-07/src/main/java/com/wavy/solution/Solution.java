2302c65283eff7778a7e8ce7fb8e124fb552a5f1
pengweiwei7 <pengweiwei9@jd.com>
2021-04-08 23:31:29 +0800

    剑指offer：斐波那契数列

---
+++ for-offer/problem-07/src/main/java/com/wavy/solution/Solution.java
@@ -0,0 +1,26 @@
+package com.wavy.solution;
+
+/**
+ * @Author pengweiwei
+ * @Date 2021/4/8 23:10
+ */
+public class Solution {
+    /**
+     * 方法一：递归
+     * 存在大量重复计算
+     *
+     * @param n
+     * @return
+     */
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(fib(n));
+    }
+}
