084c85e87e2df4cfcf03a5c6373ef0df407e191d
Wavy <w1a9v9y3@gmail.com>
2021-04-30 11:07:22 +0800

    CICT：返回倒数第 k 个节点

---
+++ ctci/ctci-11/src/main/java/com/wavy/solution/Solution.java
@@ -0,0 +1,64 @@
+package com.wavy.solution;
+
+/**
+ * @author pengweiwei
+ * @date 2021/4/30 9:57 AM
+ */
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+        }
+    }
+
+    /**
+     * 思路一：
+     * 遍历计算链表长度，然后len-k计算倒数第k个节点是整数第几个节点
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public int kthToLast(ListNode head, int k) {
+        ListNode p = head;
+        int len = 0;
+        while (null != p) {
+            len++;
+            p = p.next;
+        }
+        int num = len - k;
+        p = head;
+        while (null != p && num-- > 0) {
+            p = p.next;
+        }
+        return p.val;
+    }
+
+    /**
+     * 思路二：
+     * 双指针遍历
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public int kthToLast1(ListNode head, int k) {
+        ListNode p = head;
+        ListNode q = head;
+        while (k-- > 0) {
+            p = p.next;
+        }
+        while (null != p) {
+            q = q.next;
+            p = p.next;
+        }
+        return q.val;
+    }
+}
