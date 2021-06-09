62e10226a947d6f2c8b51ac09940b1ee7e350a22
pengweiwei7 <pengweiwei9@jd.com>
2021-06-09 23:33:27 +0800

    algorithm：特殊等价字符串组

---
+++ algorithm/serial-string/src/main/java/com/string/solution/GroupsOfSpecialEquivalentStrings.java
@@ -0,0 +1,16 @@
+package com.string.solution;
+
+/**
+ * @Author pengweiwei
+ * @Date 2021/6/9 23:10
+ */
+public class GroupsOfSpecialEquivalentStrings {
+
+    public static int numSpecialEquivGroups(String[] words) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
