529fd2c00471b88f864610baf8c51ae7a1e08c34
pengweiwei7 <pengweiwei9@jd.com>
2021-08-01 23:22:10 +0800

    data-structure：单向链表

---
+++ data-structure/src/main/java/com/ds/solution/LinkedList.java
@@ -0,0 +1,72 @@
+package com.ds.solution;
+
+/**
+ * @Author pengweiwei
+ * @Date 2021/8/1 17:01
+ */
+public class LinkedList {
+    private ListNode head;
+
+    LinkedList() {
+        this.head = new ListNode(0, null);
+    }
+
+    public void add(int val) {
+        ListNode node = new ListNode(val);
+        ListNode temp = head;
+        while (null != temp.next) {
+            temp = temp.next;
+        }
+        temp.next = node;
+        node.next = null;
+    }
+
+    public void insert(int val) {
+        ListNode node = new ListNode(val);
+        ListNode temp = head;
+        while (null != temp.next && temp.next.val <= val) {
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+
+    public void remove(int val) {
+        ListNode temp = head;
+        while (temp.next.val != val) {
+            temp = temp.next;
+        }
+        ListNode removeNode = temp.next;
+        temp.next = removeNode.next;
+        removeNode.next = null;
+    }
+
+    public void print() {
+        ListNode temp = head.next;
+        while (null != temp) {
+            System.out.printf("%d->", temp.val);
+            temp = temp.next;
+        }
+        System.out.printf("null \n");
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+        linkedList.print();
+        linkedList.add(3);
+        linkedList.print();
+        linkedList.insert(2);
+        linkedList.print();
+        linkedList.insert(4);
+        linkedList.print();
+        linkedList.add(5);
+        linkedList.print();
+        linkedList.remove(3);
+        linkedList.print();
+        linkedList.insert(6);
+        linkedList.print();
+        linkedList.remove(6);
+        linkedList.print();
+    }
+}
---
+++ data-structure/src/main/java/com/ds/solution/ListNode.java
@@ -0,0 +1,19 @@
+package com.ds.solution;
+
+/**
+ * @Author pengweiwei
+ * @Date 2021/8/1 16:56
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
